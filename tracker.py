084cd2a92196805fdc0a5ef1fa086ec2ef10588e
ayaelzein <aya.m.elzein@gmail.com>
2025-02-20 15:57:54 +0200

    monthly dictionary

---
+++ tracker.py
@@ -0,0 +1,26 @@
+
+months = {"January" : "salary_per_jan", 
+          "February" : "salary_per_feb", 
+          "March" : "salary_per_mar", 
+          "April": "salary_per_apr",
+          "May" : "salary_per_may", 
+          "June" : "salary_per_jun", 
+          "July" : "salary_per_jul", 
+          "August" : "salary_per_aug", 
+          "Septermber" : "salary_per_sep",
+          "October" : "salary_per_oct", 
+          "November" : "salary_per_nov", 
+          "December" : "salary_per_dec" }
+
+for month in months :
+  print(month)
+
+salary_per_month = [months.values]
+print(salary_per_month)
+
+# outmonth = input("Please enter the selected month: ") 
+
+# while outmonth != "stop":
+  # if outmonth == month:
+ #   salary = int(input("Please enter your salary for the month: "))
+ #   salary_per_month.append(int(salary))
